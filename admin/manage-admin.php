260735a4b8af3afb29a17defc209aa288853fe08
rithaven <veneruth@gmail.com>
2022-02-04 13:21:07 +0200

    Add Update Password Button

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -70,7 +70,7 @@
                                     <td><?php echo $username;?></td>
                                     <td>
                                         <a href="<?php echo SITEURL;?>admin/update-password.php?id=<?php echo $id; ?>" class="btn-primary">Change Password</a>
-                                        <a href="<?php echo SITEURL;?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-info">Update Admin</a>
+                                        <a href="<?php echo SITEURL;?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
                                         <a href="<?php echo SITEURL;?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a> 
                                     </td>
                                 </tr>
