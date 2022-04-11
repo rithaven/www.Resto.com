1c881151293f5de81416c6a5610e9b47f1810b4b
rithaven <veneruth@gmail.com>
2022-04-12 00:27:27 +0200

    Add,UPDATE&Delete Food Category

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -27,6 +27,36 @@
                         unset($_SESSION['delete']);
                         
                     }
+
+                    if(isset($_SESSION['no-category-found']))
+                    {
+                        echo $_SESSION['no-category-found'];
+                        unset($_SESSION['no-category-found']);
+                        
+                    }
+
+                    if(isset($_SESSION['update']))
+                    {
+                        echo $_SESSION['update'];
+                        unset($_SESSION['update']);
+                        
+                    }
+
+                    if(isset($_SESSION['upload']))
+                    {
+                        echo $_SESSION['upload'];
+                        unset($_SESSION['upload']);
+                        
+                    }
+                    if(isset($_SESSION['failed-remove']))
+                    {
+                        echo $_SESSION['failed-remove'];
+                        unset($_SESSION['failed-remove']);
+                        
+                    }
+
+
+                    
                 ?>
                 <br><br>
                <!--Button to Add Admin --->
@@ -96,7 +126,7 @@
                          
                          <td>
               
-                        <a href="#" class="btn-secondary">Update Category</a>
+                        <a href="<?php echo SITEURL; ?>admin/update-category.php?id=<?php echo $id; ?>" class="btn-secondary">Update Category</a>
                         <a href="<?php echo SITEURL; ?>admin/delete-category.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name;?>" class="btn-danger">Delete Category </a> 
                         </td>
                         </tr>
