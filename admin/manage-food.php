21cc85b4b1e1723206be0804559c3bee86fa8dd9
rithaven <veneruth@gmail.com>
2022-04-14 02:16:02 +0200

    Add Food, display food

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -20,7 +20,85 @@
                    
                }
             ?>
-        
+            <table class="tbl-full">
+                <tr>
+                        <th>S.N</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Image</th>
+                        <th>Featured</th>
+                        <th>Active</th>
+                        <th>Actions</th>
+                </tr>
+                <?php
+                    //Create a SQL Query to Get all the Food
+                    $sql = "SELECT * FROM tbl_food";
+
+                    //Execute the query
+                    $res = mysqli_query($conn, $sql);
+
+                    //Count rows to check whether we have foods or not
+                    $count = mysqli_num_rows($res);
+                     
+                    //Create Serial Number Variable and Set Default Value as 1
+                    $sn=1;
+                    if($count>0)
+                    {
+                        //We have food in Database
+                        //Get the Foods from Database and Display
+                        while($row=mysqli_fetch_assoc($res))
+                        {
+                            //get the values from individual columns
+                            $id = $row['id'];
+                            $title = $row['title'];
+                            $price = $row['price'];
+                            $image_name = $row['image_name'];
+                            $featured = $row['featured'];
+                            $active = $row['active'];
+                        ?>
+                 <tr>
+                        <td><?php echo $sn++; ?></td>
+                        <td><?php echo $title; ?></td>
+                        <td>$<?php echo $price; ?></td>
+                        <td>
+                        <?php 
+                           //Check whether we have image or not
+                           if($image_name=="")
+                           {
+                               //We do not have image, Display meassage
+                               echo "<div class='error'>Image not Added.</div>";
+
+                           }
+                           else
+                           {
+                               //WE have image, Display Image
+                               ?>
+                               <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" width="100px">
+                               <?php
+                           }
+                        
+                        ?>
+                        </td>
+                        <td><?php echo $featured; ?></td>
+                        <td><?php echo $active; ?></td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Food</a>
+                            <a href="#" class="btn-danger">Delete Food</a>
+
+                        </td>
+                    </tr>
+                    <?php
+                        }
+
+                    }
+                    else
+                    {
+                        //Food not Added in Database
+                        echo "<tr> <td colspan='7' class='error'>Food not Added Yet.</td></tr>";
+                    }
+                ?>
+                
+            </table>
             </div>
 </div>
 <?php include('partials/footer.php') ?>
